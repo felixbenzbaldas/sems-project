c4ef7dcbf267ea832734c1b1f929f5fb2ca272a5
Felix <f.benz-baldas@hotmail.de>
2024-03-14 17:52:27 +0100

    generate random string

---
+++ src/main/java/sems/RandomString.java
@@ -0,0 +1,39 @@
+package sems;
+
+import java.util.Random;
+
+public class RandomString {
+    private final String allowedCharacters;
+    private final int length;
+
+    private final Random random = new Random();
+
+    public RandomString(String allowedCharacters, int length) {
+        this.allowedCharacters = allowedCharacters;
+        this.length = length;
+    }
+
+    public RandomString() {
+        this.allowedCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+        this.length = 9;
+    }
+
+    public String next() {
+        StringBuilder stringBuilder = new StringBuilder(length);
+        int numberOfCharacters = allowedCharacters.length();
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(numberOfCharacters);
+            char randomChar = allowedCharacters.charAt(randomIndex);
+            stringBuilder.append(randomChar);
+        }
+        return stringBuilder.toString();
+    }
+
+    public long getNumberOfPossibleStrings() {
+        return (long) Math.pow(allowedCharacters.length(), length);
+    }
+
+    public double getProbabilityOfCollision(long generatedStrings) {
+        return generatedStrings / (float) getNumberOfPossibleStrings();
+    }
+}
