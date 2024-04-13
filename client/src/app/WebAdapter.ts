3c800d3bcdc597463480d91a8b6d42b478f3e6f8
Felix <f.benz-baldas@hotmail.de>
2024-04-13 17:42:12 +0200

    testCreateSemsObjectFromJson

---
+++ client/src/app/WebAdapter.ts
@@ -0,0 +1,8 @@
+import { SemsObject } from "./SemsObject";
+
+export class WebAdapter {
+  createSemsObjectFromJson(json): SemsObject {
+    return new SemsObject();
+  }
+
+}
--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,5 +1,24 @@
-describe('testInstallation', () => {
-  it('test', () => {
-    expect(true).toBe(true);
+import { WebAdapter } from "./WebAdapter";
+import { SemsObject } from "./SemsObject";
+
+describe('app', () => {
+
+  it('test create SemsObject from json', () => {
+    let jsonSemsObject = {
+      "id":"1-cmcCvfEhP2",
+      "details":[],
+      "properties":
+        {
+          "text":"Beispiel",
+          "context":null,
+          "isPrivate":false,
+          "defaultExpanded":true
+        }
+    };
+    let webAdapter : WebAdapter = new WebAdapter();
+    let semsObject : SemsObject = webAdapter.createSemsObjectFromJson(jsonSemsObject);
+    expect(semsObject).toBeTruthy();
   });
+
+
 });
