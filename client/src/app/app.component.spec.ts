c88d760ad7a757d07dbf239011ee4cba5abe802d
Felix <f.benz-baldas@hotmail.de>
2024-05-01 09:02:57 +0200

    formatting

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -8,68 +8,68 @@ import {SemsObject} from "./SemsObject";
 
 describe('app', () => {
 
-  it('can create SemsObject from json', () => {
-    let jsonSemsObject = {
-      "id":"1-abc",
-      "details":["1-345"],
-      "text":"Beispiel",
-      "context":null,
-      "isPrivate":false,
-      "defaultExpanded":true
-    };
-    let webAdapter : WebAdapter = new WebAdapter();
+    it('can create SemsObject from json', () => {
+        let jsonSemsObject = {
+            "id": "1-abc",
+            "details": ["1-345"],
+            "text": "Beispiel",
+            "context": null,
+            "isPrivate": false,
+            "defaultExpanded": true
+        };
+        let webAdapter: WebAdapter = new WebAdapter();
 
-    let semsObject : SemsObject = webAdapter.createSemsObjectFromJson(jsonSemsObject);
+        let semsObject: SemsObject = webAdapter.createSemsObjectFromJson(jsonSemsObject);
 
-    expect(semsObject).toBeTruthy();
-  });
-
-  it('can add detail', async () => {
-    let semsObject : SemsObject = new SemsObjectImpl();
-    let addressOfDetail = new SemsAddress();
-
-    await semsObject.addDetail(addressOfDetail);
-
-    expect(semsObject.getDetails()[0]).toBe(addressOfDetail);
-  });
-
-  it('can create remote SemsObject', async () => {
-    let createdSemsObject : SemsObject = new SemsObjectImpl();
-    let semsLocationMock = {} as SemsLocation;
-    semsLocationMock.createSemsObject = jest.fn().mockImplementation(
-        (semsHouse : SemsHouse) => {
-          return lastValueFrom(of(createdSemsObject));
+        expect(semsObject).toBeTruthy();
     });
-    let semsHouse = new SemsHouse(semsLocationMock);
 
-    let semsObject = await semsHouse.createSemsObject();
+    it('can add detail', async () => {
+        let semsObject: SemsObject = new SemsObjectImpl();
+        let addressOfDetail = new SemsAddress();
 
-    expect(semsObject).toBeTruthy();
-    expect(semsLocationMock.createSemsObject).toHaveBeenCalledWith(semsHouse);
-  });
+        await semsObject.addDetail(addressOfDetail);
 
-  it('can observe SemsObject', done  => {
-    let semsObject : SemsObject = new SemsObjectImpl();
-    semsObject.subscribe({
-      next: value => {
-        try {
-          expect(value).toEqual("addedDetail");
-          done();
-        } catch (error) {
-          done(error);
-        }
-      }
+        expect(semsObject.getDetails()[0]).toBe(addressOfDetail);
     });
-    semsObject.addDetail(new SemsAddress());
-  });
 
-  it('can parse SemsAddress', () => {
-    let addressString = "1-abc";
+    it('can create remote SemsObject', async () => {
+        let createdSemsObject: SemsObject = new SemsObjectImpl();
+        let semsLocationMock = {} as SemsLocation;
+        semsLocationMock.createSemsObject = jest.fn().mockImplementation(
+            (semsHouse: SemsHouse) => {
+                return lastValueFrom(of(createdSemsObject));
+            });
+        let semsHouse = new SemsHouse(semsLocationMock);
 
-    let semsAddress = SemsAddress.parse(addressString);
+        let semsObject = await semsHouse.createSemsObject();
 
-    expect(semsAddress.getHouse()).toEqual("1");
-    expect(semsAddress.getName()).toEqual("abc");
-  });
+        expect(semsObject).toBeTruthy();
+        expect(semsLocationMock.createSemsObject).toHaveBeenCalledWith(semsHouse);
+    });
+
+    it('can observe SemsObject', done => {
+        let semsObject: SemsObject = new SemsObjectImpl();
+        semsObject.subscribe({
+            next: value => {
+                try {
+                    expect(value).toEqual("addedDetail");
+                    done();
+                } catch (error) {
+                    done(error);
+                }
+            }
+        });
+        semsObject.addDetail(new SemsAddress());
+    });
+
+    it('can parse SemsAddress', () => {
+        let addressString = "1-abc";
+
+        let semsAddress = SemsAddress.parse(addressString);
+
+        expect(semsAddress.getHouse()).toEqual("1");
+        expect(semsAddress.getName()).toEqual("abc");
+    });
 
 });
