3c9c806574174bde47919e4953fbdafb70dda6b1
Felix <f.benz-baldas@hotmail.de>
2024-08-09 17:39:29 +0200

    TestApp

---
+++ src/test/java/nodomain/sems/TestApp.java
@@ -0,0 +1,33 @@
+package nodomain.sems;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Comparator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestApp {
+
+    private static final String TEST_RESOURCES_PATH = "./src/test/resources";
+    private static final String PATH_FOR_TMP_FILES = TEST_RESOURCES_PATH + "/tmp";
+
+    @BeforeEach
+    void beforeEach() {
+        new File(PATH_FOR_TMP_FILES).mkdirs();
+    }
+
+    @AfterEach
+    void afterEach() throws IOException {
+        deleteDirectory(new File(PATH_FOR_TMP_FILES));
+    }
+
+    static private void deleteDirectory(File file) throws IOException {
+        Files.walk(file.toPath()).sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
+    }
+}
