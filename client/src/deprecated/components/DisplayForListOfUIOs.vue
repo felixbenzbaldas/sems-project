aa550d718d9a434e98fa0f77fba8e9347f3d1200
Felix <f.benz-baldas@hotmail.de>
2024-08-10 09:39:31 +0200

    comment out deprecated code

--- client/src/deprecated/components/DisplayForListOfUIOs.vue
+++ client/src/deprecated/components/DisplayForListOfUIOs.vue
@@ -1,34 +1,34 @@
-<script setup lang="ts">
-import {ref} from "vue";
-import type {ObservableList} from "@/deprecated/core/ObservableList";
-import type {App} from "@/deprecated/core/App";
-import type {UserInterfaceObject} from "@/deprecated/user-interface/UserInterfaceObject";
-import type {UserInterface} from "@/deprecated/user-interface/UserInterface";
+<!--<script setup lang="ts">-->
+<!--import {ref} from "vue";-->
+<!--import type {ObservableList} from "@/deprecated/core/ObservableList";-->
+<!--import type {App} from "@/deprecated/core/App";-->
+<!--import type {UserInterfaceObject} from "@/deprecated/user-interface/UserInterfaceObject";-->
+<!--import type {UserInterface} from "@/deprecated/user-interface/UserInterface";-->
 
-const props = defineProps<{
-    list: ObservableList<UserInterfaceObject>,
-    userInterface : UserInterface,
-    app: App
-}>();
+<!--const props = defineProps<{-->
+<!--    list: ObservableList<UserInterfaceObject>,-->
+<!--    userInterface : UserInterface,-->
+<!--    app: App-->
+<!--}>();-->
 
-const listObject = ref([]);
+<!--const listObject = ref([]);-->
 
-function updateListObject() {
-    listObject.value = props.list.createCopyOfList();
-}
+<!--function updateListObject() {-->
+<!--    listObject.value = props.list.createCopyOfList();-->
+<!--}-->
 
-props.list.subject.subscribe(next => {
-    updateListObject();
-});
+<!--props.list.subject.subscribe(next => {-->
+<!--    updateListObject();-->
+<!--});-->
 
-updateListObject();
+<!--updateListObject();-->
 
-</script>
+<!--</script>-->
 
-<template>
-    <GUIForUIO style="margin: 0.5rem" v-for="uio in listObject" :uio="uio" :app="props.app" :userInterface="props.userInterface" />
-    <!-- TODO: :key verwenden  -->
-</template>
+<!--<template>-->
+<!--    <GUIForUIO style="margin: 0.5rem" v-for="uio in listObject" :uio="uio" :app="props.app" :userInterface="props.userInterface" />-->
+<!--    &lt;!&ndash; TODO: :key verwenden  &ndash;&gt;-->
+<!--</template>-->
 
-<style scoped>
-</style>
+<!--<style scoped>-->
+<!--</style>-->
--- client/src/deprecated/components/GUIForUIO.vue
+++ client/src/deprecated/components/GUIForUIO.vue
@@ -1,75 +1,75 @@
-<script setup lang="ts">
-import {type Ref, ref} from "vue";
-import ListOfSemsObjects from "@/deprecated/components/DisplayForListOfUIOs.vue";
-import HeaderBody from "@/deprecated/components/HeaderBody.vue";
-import {ObservableList} from "@/deprecated/core/ObservableList";
-import type {App} from "@/deprecated/core/App";
-import {UserInterfaceObject} from "@/deprecated/user-interface/UserInterfaceObject";
-import type {UserInterface} from "@/deprecated/user-interface/UserInterface";
+<!--<script setup lang="ts">-->
+<!--import {type Ref, ref} from "vue";-->
+<!--import ListOfSemsObjects from "@/deprecated/components/DisplayForListOfUIOs.vue";-->
+<!--import HeaderBody from "@/deprecated/components/HeaderBody.vue";-->
+<!--import {ObservableList} from "@/deprecated/core/ObservableList";-->
+<!--import type {App} from "@/deprecated/core/App";-->
+<!--import {UserInterfaceObject} from "@/deprecated/user-interface/UserInterfaceObject";-->
+<!--import type {UserInterface} from "@/deprecated/user-interface/UserInterface";-->
 
-const props = defineProps<{
-    uio: UserInterfaceObject,
-    app: App,
-    userInterface: UserInterface,
-}>();
+<!--const props = defineProps<{-->
+<!--    uio: UserInterfaceObject,-->
+<!--    app: App,-->
+<!--    userInterface: UserInterface,-->
+<!--}>();-->
 
-const hasBody = ref(false);
-const list : Ref<ObservableList<UserInterfaceObject>> = ref(undefined);
-list.value = new ObservableList<UserInterfaceObject>();
+<!--const hasBody = ref(false);-->
+<!--const list : Ref<ObservableList<UserInterfaceObject>> = ref(undefined);-->
+<!--list.value = new ObservableList<UserInterfaceObject>();-->
 
-async function updateHasBody() {
-    hasBody.value = !props.uio.getSemsObject().getDetails().isEmpty();
-};
+<!--async function updateHasBody() {-->
+<!--    hasBody.value = !props.uio.getSemsObject().getDetails().isEmpty();-->
+<!--};-->
 
-async function init() {
-    updateHasBody();
-    updateDetails();
-    let details = props.uio.getSemsObject().getDetails();
-    details.subject.subscribe(next => {
-        updateHasBody();
-        updateDetails();
-    });
-}
+<!--async function init() {-->
+<!--    updateHasBody();-->
+<!--    updateDetails();-->
+<!--    let details = props.uio.getSemsObject().getDetails();-->
+<!--    details.subject.subscribe(next => {-->
+<!--        updateHasBody();-->
+<!--        updateDetails();-->
+<!--    });-->
+<!--}-->
 
-init();
+<!--init();-->
 
-async function updateDetails() {
-    list.value.clear();
-    for (let detailPath of props.uio.getSemsObject().getDetails().createCopyOfList()) {
-        let semsObject = await props.app.getLocation().getObject(detailPath);
-        let uio = new UserInterfaceObject(props.userInterface);
-        uio.setSemsObject(semsObject);
-        list.value.add(uio);
-    }
-}
+<!--async function updateDetails() {-->
+<!--    list.value.clear();-->
+<!--    for (let detailPath of props.uio.getSemsObject().getDetails().createCopyOfList()) {-->
+<!--        let semsObject = await props.app.getLocation().getObject(detailPath);-->
+<!--        let uio = new UserInterfaceObject(props.userInterface);-->
+<!--        uio.setSemsObject(semsObject);-->
+<!--        list.value.add(uio);-->
+<!--    }-->
+<!--}-->
 
-function saveChanges(event: any) {
-    props.uio.getSemsObject().setText(event.target.innerText.trim());
-}
+<!--function saveChanges(event: any) {-->
+<!--    props.uio.getSemsObject().setText(event.target.innerText.trim());-->
+<!--}-->
 
-</script>
+<!--</script>-->
 
-<template>
-    <HeaderBody>
-        <template #head>
-            <div style="display: inline-block ; max-width: 20rem">
-                <div @click="$event.stopPropagation()" class="contentEditableDiv" contenteditable @blur="saveChanges">
-                    {{ props.uio.getSemsObject().getText() }}
-                </div>
-            </div>
-        </template>
-        <template v-if="hasBody" #body>
-            <DisplayForListOfUIOs :list="list" :app="props.app" :userInterface="props.userInterface"/>
-        </template>
-    </HeaderBody>
-</template>
+<!--<template>-->
+<!--    <HeaderBody>-->
+<!--        <template #head>-->
+<!--            <div style="display: inline-block ; max-width: 20rem">-->
+<!--                <div @click="$event.stopPropagation()" class="contentEditableDiv" contenteditable @blur="saveChanges">-->
+<!--                    {{ props.uio.getSemsObject().getText() }}-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </template>-->
+<!--        <template v-if="hasBody" #body>-->
+<!--            <DisplayForListOfUIOs :list="list" :app="props.app" :userInterface="props.userInterface"/>-->
+<!--        </template>-->
+<!--    </HeaderBody>-->
+<!--</template>-->
 
-<style scoped>
+<!--<style scoped>-->
 
-.contentEditableDiv {
-    min-height: 1rem;
-    min-width: 1rem;
-    background-color: antiquewhite;
-}
+<!--.contentEditableDiv {-->
+<!--    min-height: 1rem;-->
+<!--    min-width: 1rem;-->
+<!--    background-color: antiquewhite;-->
+<!--}-->
 
-</style>
+<!--</style>-->
