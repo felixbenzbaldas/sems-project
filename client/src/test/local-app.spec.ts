8e366edf6a9a6de6b3aa5a965952a780fb9561d7
Felix <f.benz-baldas@hotmail.de>
2024-08-11 21:16:56 +0200

    minor

--- client/src/test/local-app.spec.ts
+++ client/src/test/local-app.spec.ts
@@ -1,8 +1,9 @@
-import {describe, expect, it} from "vitest";
+import {describe, expect, it, test} from "vitest";
 import {Identity} from "@/Identity";
 import {Starter} from "@/Starter";
 
-let testServer = 'http://localhost:8081/';
+let testPort = 8081;
+let testServer = 'http://localhost:' + testPort + '/';
 
 /// The local app includes a running local server. These tests need a running test-server.
 /// We do not use mocks here, because the local server is seen as part of the application.
