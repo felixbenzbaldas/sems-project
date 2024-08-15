b1078ceb78158001c6db96b8e6728798c9710dfe
Felix <f.benz-baldas@hotmail.de>
2024-08-15 17:26:06 +0200

    replace in deployment

--- client/src/Starter.ts
+++ client/src/Starter.ts
@@ -43,7 +43,7 @@ export class Starter {
                 )
             ),
             app.createText(''),
-            app.createText('marker-a1a'),
+            app.createText('marker-dr53hifhh4'),
         );
         return app;
     }
