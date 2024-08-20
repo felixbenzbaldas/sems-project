b88e40dc46098e77ef102f7d222ea975072a97c8
Felix <f.benz-baldas@hotmail.de>
2024-08-20 09:51:02 +0200

    improve structure of tests

--- client/src/test/abstract-ui.spec.ts
+++ client/src/test/abstract-ui.spec.ts
@@ -2,7 +2,7 @@ import {describe, expect, it} from "vitest";
 import {Identity} from "@/Identity";
 import {Starter} from "@/Starter";
 
-describe('abstract ui for local app', () => {
+describe('abstract ui', () => {
 
     it('can create object', async () => {
         let app : Identity = Starter.createAppWithUI();
@@ -10,6 +10,23 @@ describe('abstract ui for local app', () => {
         await app.abstractUi.defaultAction();
 
         expect(app.abstractUi.content.list.jsList.length).toBe(1);
+    });
+
+    it('can get json', async () => {
+        let app : Identity = Starter.createAppWithUI();
+
+        let json = app.json();
+
+        expect(json.content.list).toEqual([]);
+    });
+
+    it('can export', async () => {
+        let app : Identity = Starter.createAppWithUI();
+        expect(app.abstractUi.output.getUi().hidden).toBe(true);
+
+        app.abstractUi.export();
+
+        expect(app.abstractUi.output.getUi().hidden).toBe(false);
     });
 
 });
