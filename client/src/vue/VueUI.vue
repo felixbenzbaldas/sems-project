301ed67c35b37d0793351d8a5c90b3bed51bdcfa
Felix <f.benz-baldas@hotmail.de>
2024-08-20 08:28:43 +0200

    minor

--- client/src/vue/VueUI.vue
+++ client/src/vue/VueUI.vue
@@ -66,7 +66,7 @@ function saveText(event: any) {
         <div v-if="identity.abstractUi?.commands" style="margin-bottom: 0.5rem; padding-bottom: 0.5rem; border-bottom: dashed">
             <VueUI :identity="identity.abstractUi.commands" :is-view="true"/>
         </div>
-        <VueUI v-if="identity.abstractUi" :identity="props.identity.abstractUi.output.getUi()"></VueUI>
+        <VueUI v-if="identity.abstractUi" :identity="props.identity.abstractUi.output.getUi()" :is-view="true"></VueUI>
         <VueUI v-if="identity.abstractUi" :identity="identity.abstractUi.content" :is-view="identity.abstractUi.isWebsite"/>
         <button v-else-if="identity.action" @click="identity.action()" style="margin: 0.3rem">
             {{text}}
