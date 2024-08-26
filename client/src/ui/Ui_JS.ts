1c5d79326e6d71ce14efe1b203b089e39a35626a
Felix <f.benz-baldas@hotmail.de>
2024-08-26 21:43:51 +0200

    Ui_JS

--- client/src/ui/Ui_JS.ts
+++ client/src/ui/Ui_JS.ts
@@ -10,13 +10,23 @@ export class Ui_JS {
     getUi(): HTMLElement {
         if (this.identity.appA?.ui) {
             this.div.appendChild(this.identity.appA.ui.content.ui_js.getUi());
-        } else if (this.identity.list) {
-            this.identity.list.jsList.forEach((currentIdentity) => {
-                this.div.appendChild(currentIdentity.ui_js.getUi());
-            });
         } else if (this.neitherNullNorUndefined(this.identity.text)) {
-            this.div.innerText = this.identity.text;
-            this.div.style.minHeight = '1rem';
+            let textElement = document.createElement('div');
+            textElement.innerText = this.identity.text;
+            textElement.style.minHeight = '1rem';
+            this.div.appendChild(textElement);
+            if (this.identity.list && this.identity.list.jsList.length > 0) {
+                let list = document.createElement('div');
+                list.style.marginLeft = '0.8rem';
+                for (let current of this.identity.list.jsList) {
+                    list.appendChild(current.ui_js.getUi());
+                }
+                this.div.appendChild(list);
+            }
+        } else if (this.identity.list) {
+            this.identity.list.jsList.forEach((current) => {
+                this.div.appendChild(current.ui_js.getUi());
+            });
         }
         return this.div;
     }
