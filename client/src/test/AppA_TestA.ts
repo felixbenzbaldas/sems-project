9c0aff7429e700ac59ba4a573011f8f52c3725da
Felix <f.benz-baldas@hotmail.de>
2024-09-05 10:54:39 +0200

    tester

--- client/src/test/AppA_TestA.ts
+++ client/src/test/AppA_TestA.ts
@@ -3,11 +3,11 @@ import type {Entity} from "@/Entity";
 export class AppA_TestA {
 
     constructor(private entity : Entity) {
-        this.createSpecifications();
+        this.createResults();
     }
 
-    createSpecifications() {
+    createResults() {
         this.entity.appA.ui.content.list.add(this.entity.appA.simple_createText(
-            'Specification: This specification appears, when visiting test page.'));
+            '[OK] The tester can show this text.'));
     }
 }
