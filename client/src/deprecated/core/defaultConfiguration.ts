4055bc4dd980f454314658a2c9a301694fd44ce7
Felix <f.benz-baldas@hotmail.de>
2024-09-14 20:11:04 +0200

    remove deprecated code; remove dependency rxjs

--- client/src/deprecated/core/defaultConfiguration.ts
+++
@@ -1,4 +0,0 @@
-export const defaultConfiguration = {
-    server: 'http://localhost:8081/',
-    writingPosition: ['house1']
-}
--- client/src/deprecated/websiteData.ts
+++
@@ -1,7 +0,0 @@
-export const websiteData = {
-    workingPlace: [
-        {
-            text: 'Hello World!',
-        }
-    ]
-}
