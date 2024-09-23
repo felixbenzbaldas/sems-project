2ffd02f49c59a4842985f25c832a73d09848847b
Felix <f.benz-baldas@hotmail.de>
2024-09-23 15:34:24 +0200

    deployment with java

--- src/main/java/nodomain/simple/Starter.java
+++ src/main/java/nodomain/simple/Starter.java
@@ -37,8 +37,33 @@ public class Starter {
             Utils.delete(new File(deploymentPath + "/heroku/sems/index.html"));
             Utils.delete(new File(deploymentPath + "/heroku/sems/diko-thesis-2017.pdf"));
             Utils.copyFolder(Path.of("client/dist"), Path.of(deploymentPath + "/heroku/sems"));
+
+            String replacementPathAboutHeader = deploymentPath + "/PUBLIC-replacement-about-header.txt";
+            String replacementPathAboutBody = deploymentPath + "/PUBLIC-replacement-about-body.txt";
+            String replacementPathImpressumHeader = deploymentPath + "/PUBLIC-replacement-impressum-header.txt";
+            String replacementPathImpressumBody= deploymentPath + "/PUBLIC-replacement-impressum-body.txt";
+
+            deployment_replace(deploymentPath, replacementPathAboutHeader, "marker-dr53hifhh4-about-header");
+            deployment_replace(deploymentPath, replacementPathAboutBody, "marker-dr53hifhh4-about-body");
+            deployment_replace(deploymentPath, replacementPathImpressumHeader, "marker-dr53hifhh4-impressum-header");
+            deployment_replace(deploymentPath, replacementPathImpressumBody, "marker-dr53hifhh4-impressum-body");
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private static void deployment_replace(String deploymentPath, String pathOfReplacement, String toReplace) throws IOException {
+        String replacement = Utils.readFromFile(new File(pathOfReplacement));
+        boolean found = false;
+        for (File file : new File(deploymentPath + "/heroku/sems/assets").listFiles()) {
+            String oldText = Utils.readFromFile(file);
+            if (oldText.contains(toReplace)) {
+                found = true;
+            }
+            Utils.writeToFile(file, oldText.replace(toReplace, replacement));
+        }
+        if (!found) {
+            throw new RuntimeException("replace was not successful!");
         }
     }
 
