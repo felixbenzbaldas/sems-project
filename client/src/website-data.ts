d7911ecaee2aa0a16976edc357e50b9f3596025b
Felix <f.benz-baldas@hotmail.de>
2024-09-24 11:40:20 +0200

    websiteData (demoWebsite)

---
+++ client/src/website-data.ts
@@ -0,0 +1,69 @@
+export const websiteData = {
+    "text": "demo website",
+    "list": [
+        [
+            "0"
+        ],
+        [
+            "1"
+        ],
+        [
+            "2"
+        ],
+        [
+            "4"
+        ],
+        [
+            "5"
+        ],
+        [
+            "7"
+        ],
+        [
+            "8"
+        ]
+    ],
+    "objects": {
+        "0": {
+            "text": "Hello! This is the demo website. It will be replaced in the deployment."
+        },
+        "1": {
+            "text": ""
+        },
+        "2": {
+            "text": "collapsible parent",
+            "list": [
+                [
+                    "..",
+                    "3"
+                ]
+            ],
+            "collapsible": true,
+            "collapsed": false
+        },
+        "3": {
+            "text": "subitem"
+        },
+        "4": {
+            "text": ""
+        },
+        "5": {
+            "text": "non-collapsible parent",
+            "list": [
+                [
+                    "..",
+                    "6"
+                ]
+            ]
+        },
+        "6": {
+            "text": "another subitem"
+        },
+        "7": {
+            "text": ""
+        },
+        "8": {
+            "text": "and a link: tbd"
+        }
+    }
+};
