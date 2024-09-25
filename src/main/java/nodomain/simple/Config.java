f9c5a5ef3c49e2f8960860a7e5f9827a16b535c3
Felix <f.benz-baldas@hotmail.de>
2024-09-25 16:31:46 +0200

    Config.java

---
+++ src/main/java/nodomain/simple/Config.java
@@ -0,0 +1,9 @@
+package nodomain.simple;
+
+/**
+ * Adapt this class locally and mark it as "Assume Unchanged". (in TortoiseGit)
+ */
+public class Config {
+    public static String pathToConfigFile;
+
+}
