2c2f72c9002d5b18687f523b8e1c7a50f1e9312d
Felix <f.benz-baldas@hotmail.de>
2024-10-05 21:27:19 +0200

    minor

--- client/src/website-data.ts
+++ client/src/website-data.ts
@@ -64,7 +64,7 @@ export const websiteData = {
         },
         "8": {
             "text": "Home",
-            "link": "https://.einfaches-web.org"
+            "link": "https://einfaches-web.org"
         }
     }
 };
