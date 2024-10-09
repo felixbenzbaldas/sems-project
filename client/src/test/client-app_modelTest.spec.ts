6dd1102c6834cfc99c3a454d286f7f0d42d861d9
Felix <f.benz-baldas@hotmail.de>
2024-10-09 16:54:01 +0200

    extraObjectForUi -> adapt a test

--- client/src/test/client-app_modelTest.spec.ts
+++ client/src/test/client-app_modelTest.spec.ts
@@ -24,14 +24,15 @@ describe('client-app (modelTests)', () => {
     });
 
     it('focuses clicked object', async () => {
+        app.appA.uiA.setExtraObjectForUi(true);
         await app.appA.uiA.globalEventG.defaultAction();
-        app.appA.uiA.focused.text = 'marker-foo';
+        app.appA.uiA.focused.getObject().text = 'marker-foo';
         app.appA.uiA.focused = undefined;
         await app.appA.uiA.update();
 
         await ui.click('marker-foo');
 
-        expect(app.appA.uiA.focused.text).toEqual('marker-foo');
+        expect(app.appA.uiA.focused.getObject().text).toEqual('marker-foo');
     });
 
 });
