352a722170536783e73950c193057a725652f470
Felix <f.benz-baldas@hotmail.de>
2024-10-15 13:56:09 +0200

    create testData

---
+++ client/src/testData.ts
@@ -0,0 +1,110 @@
+export const testData = {
+    "text": "demo website (container)",
+    "list": [
+        [
+            "20"
+        ]
+    ],
+    "objects": {
+        "0": {
+            "text": "Hello! This is the demo website. It will be replaced in the deployment."
+        },
+        "1": {
+            "text": ""
+        },
+        "2": {
+            "text": "collapsible parent",
+            "list": [
+                [
+                    "..",
+                    "3"
+                ]
+            ],
+            "collapsible": true,
+            "collapsed": true
+        },
+        "3": {
+            "text": "subitem"
+        },
+        "4": {
+            "text": ""
+        },
+        "5": {
+            "text": "non-collapsible parent",
+            "list": [
+                [
+                    "..",
+                    "6"
+                ]
+            ]
+        },
+        "6": {
+            "text": "another subitem"
+        },
+        "7": {
+            "text": ""
+        },
+        "8": {
+            "text": "Home",
+            "link": "https://einfaches-web.org"
+        },
+        "20": {
+            "text": "localhost",
+            "list": [
+                [
+                    "..",
+                    "21"
+                ],
+                [
+                    "..",
+                    "FObPP80aX9"
+                ]
+            ]
+        },
+        "21": {
+            "text": "start",
+            "list": [
+                [
+                    "..",
+                    "0"
+                ],
+                [
+                    "..",
+                    "1"
+                ],
+                [
+                    "..",
+                    "2"
+                ],
+                [
+                    "..",
+                    "4"
+                ],
+                [
+                    "..",
+                    "5"
+                ],
+                [
+                    "..",
+                    "7"
+                ],
+                [
+                    "..",
+                    "8"
+                ]
+            ]
+        },
+        "FObPP80aX9": {
+            "text": "webMeta",
+            "list": [
+                [
+                    "..",
+                    "p5L6xkXZAO"
+                ]
+            ]
+        },
+        "p5L6xkXZAO": {
+            "text": "some meta information"
+        }
+    }
+};
