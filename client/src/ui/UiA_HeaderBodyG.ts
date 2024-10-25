4f31c0982e462ef6a7218e9cac6e662790420f7c
Felix <f.benz-baldas@hotmail.de>
2024-10-25 15:41:47 +0200

    simplify

--- client/src/ui/UiA_HeaderBodyG.ts
+++ client/src/ui/UiA_HeaderBodyG.ts
@@ -1,4 +1,3 @@
-import {notNullUndefined, nullUndefined} from "@/utils";
 import type {Entity} from "@/Entity";
 
 export class UiA_HeaderBodyG {
@@ -13,29 +12,11 @@ export class UiA_HeaderBodyG {
             await this.entity.uiA.bodyG.update();
             this.entity.uiA.htmlElement.appendChild(this.entity.uiA.headerG.htmlElement);
             this.entity.uiA.htmlElement.appendChild(this.entity.uiA.bodyG.htmlElement);
-        } else if (this.isTextWithList()) {
-            this.entity.uiA.headerG.update();
-            await this.entity.uiA.bodyG.update();
-            this.entity.uiA.htmlElement.appendChild(this.entity.uiA.headerG.htmlElement);
-            this.entity.uiA.htmlElement.appendChild(this.entity.uiA.bodyG.htmlElement);
-        } else if (this.isPlainText()) {
-            this.entity.uiA.headerG.update();
-            await this.entity.uiA.bodyG.update();
-            this.entity.uiA.htmlElement.appendChild(this.entity.uiA.headerG.htmlElement);
-            this.entity.uiA.htmlElement.appendChild(this.entity.uiA.bodyG.htmlElement);
-        } else if (this.entity.uiA.getObject().testRunA) {
+        } else {
             this.entity.uiA.headerG.update();
             await this.entity.uiA.bodyG.update();
             this.entity.uiA.htmlElement.appendChild(this.entity.uiA.headerG.htmlElement);
             this.entity.uiA.htmlElement.appendChild(this.entity.uiA.bodyG.htmlElement);
         }
-    }
-
-    isPlainText() {
-        return notNullUndefined(this.entity.uiA.getObject().text) && !this.entity.uiA.getObject().listA;
-    }
-
-    isTextWithList() {
-        return notNullUndefined(this.entity.uiA.getObject().text) && this.entity.uiA.getObject().listA;
     }
 }
