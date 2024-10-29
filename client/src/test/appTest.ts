3dc8064b3a09aefba1fc62de8e3a0fe65025de3b
Felix <f.benz-baldas@hotmail.de>
2024-10-29 15:05:14 +0100

    minor

--- client/src/test/appTest.ts
+++ client/src/test/appTest.ts
@@ -5,10 +5,8 @@ export const appTest = (app : Entity) => {
     let test = app.createFormalText('appTest', (run : Entity) => {
     });
     test.testG_installNestedTestsA();
-    {
-        test.testG_nestedTestsA.add('failingDummyTest', (run : Entity) => {
-            assert(false);
-        });
-    }
+    test.testG_nestedTestsA.add('failingDummyTest', (run : Entity) => {
+        assert(false);
+    });
     return test;
 }
