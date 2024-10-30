312c330415afec6183d49f2c5df0f0c745d39508
Felix <f.benz-baldas@hotmail.de>
2024-10-30 16:57:10 +0100

    simplify key definition

--- client/src/ui/AppA_UiA_KeyG.ts
+++ client/src/ui/AppA_UiA_KeyG.ts
@@ -6,52 +6,52 @@ export class AppA_UiA_KeyG {
     map : Map<string, (keyboardEvent : KeyboardEvent) => Promise<void>> = new Map();
 
     constructor(private entity: Entity) {
-        this.map.set('Enter (keyup)', async keyboardEvent => {
+        this.map.set('Enter', async keyboardEvent => {
             this.entity.appA.uiA.focused.uiA.textG.save();
             await this.entity.appA.uiA.globalEventG.defaultAction();
         });
-        this.map.set('alt+Enter (keyup)', async keyboardEvent => {
+        this.map.set('alt+Enter', async keyboardEvent => {
             this.entity.appA.uiA.focused.uiA.textG.save();
             await this.entity.appA.uiA.globalEventG.newSubitem();
         });
-        this.map.set('ctrl+f (keydown)', async keyboardEvent => {
-            keyboardEvent.preventDefault();
-        });
-        this.map.set('ctrl+f (keyup)', async keyboardEvent => {
+        this.map.set('ctrl+f', async keyboardEvent => {
             await this.entity.appA.uiA.globalEventG.toggleCollapsible();
         });
-        this.map.set('ctrl+f (keyup)', async keyboardEvent => {
+        this.map.set('ctrl+f', async keyboardEvent => {
             await this.entity.appA.uiA.globalEventG.toggleCollapsible();
         });
-        this.map.set('ctrl+i (keydown)', async keyboardEvent => {
-            keyboardEvent.preventDefault();
-        });
-        this.map.set('ctrl+i (keyup)', async keyboardEvent => {
+        this.map.set('ctrl+i', async keyboardEvent => {
             await this.entity.appA.uiA.globalEventG.scaleDown();
         });
-        this.map.set('ctrl+k (keydown)', async keyboardEvent => {
-            keyboardEvent.preventDefault();
-        });
-        this.map.set('ctrl+k (keyup)', async keyboardEvent => {
+        this.map.set('ctrl+k', async keyboardEvent => {
             await this.entity.appA.uiA.globalEventG.scaleUp();
         });
     }
 
     async keyboardEvent(keyboardEvent: KeyboardEvent) {
-        let keyCompareString = this.createCompareString(keyboardEvent);
+        let compareString = this.createCompareString(keyboardEvent);
         if (this.entity.appA.testMode) {
-            this.entity.logInfo(keyCompareString);
+            this.entity.logInfo(compareString);
         }
-        if (this.map.has(keyCompareString)) {
-            await this.map.get(keyCompareString)(keyboardEvent);
+        if (this.map.has(compareString)) {
+            await this.map.get(compareString)(keyboardEvent);
+            keyboardEvent.preventDefault();
+        }
+        let compareString_withoutType = this.createCompareString_withoutType(keyboardEvent);
+        if (this.map.has(compareString_withoutType)) {
+            if (keyboardEvent.type === 'keyup') {
+                await this.map.get(compareString_withoutType)(keyboardEvent);
+            } else {
+                keyboardEvent.preventDefault();
+            }
         }
     }
 
-    createCompareString(keyboardEvent: KeyboardEvent) {
+    createCompareString(keyboardEvent: KeyboardEvent) : string {
         return this.createCompareString_withoutType(keyboardEvent) + ' (' + keyboardEvent.type + ')';
     }
 
-    createCompareString_withoutType(keyboardEvent: KeyboardEvent) {
+    createCompareString_withoutType(keyboardEvent: KeyboardEvent) : string {
         let list = [];
         if (keyboardEvent.ctrlKey) {
             list.push("ctrl");
