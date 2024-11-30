3887617fa074560d9797e38e978ba8f43a54603b
Felix <f.benz-baldas@hotmail.de>
2024-11-30 11:14:45 +0100

    simplify

--- client/src/VueStarter.vue
+++ client/src/VueStarter.vue
@@ -4,9 +4,7 @@ import {Environment} from "@/Environment";
 
 async function init() {
     let html = await Environment.create().createApp().appA.createStarter().fullStart();
-    while (document.body.children.length > 0) {
-        document.body.removeChild(document.body.children.item(0));
-    }
+    document.body.innerHTML = null;
     document.body.appendChild(html);
 }
 
