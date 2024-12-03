a4daa8cc16f096c62a9e98280a1a9156b5d6b23e
Felix <f.benz-baldas@hotmail.de>
2024-12-03 16:08:22 +0100

    simplify

--- client/src/StarterA.ts
+++ client/src/StarterA.ts
@@ -48,8 +48,7 @@ export class StarterA {
     async run() : Promise<Entity> {
         this.createTester2(this.getEnvironment().testCreator)
         let pathParam : string = this.getEnvironment().queryParams.get('run');
-        let path = this.createdApp.appA.createPath(pathParam.split('_'));
-        let run : Entity = await (await this.createdApp.deprecated_resolve(path)).testG_run();
+        let run : Entity = await (await this.createdApp.resolveListOfNames(pathParam.split('_'))).testG_run();
         this.createdApp.appA.uiA.content.listA.jsList.push(run);
         return this.createdApp;
     }
