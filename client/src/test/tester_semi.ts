2b41503d9e276b445cf1813989279c733331f373
Felix <f.benz-baldas@hotmail.de>
2024-12-11 11:13:24 +0100

    extract tests in own files

---
+++ client/src/test/tester_semi.ts
@@ -0,0 +1,113 @@
+import type {TestG_NestedTestsA} from "@/test/TestG_NestedTestsA";
+import {assert, assert_sameAs, assertFalse, downloadText, setWidth, textFileInput} from "@/utils";
+
+export function tester_semi_add(tests : TestG_NestedTestsA) {
+    tests.addNestedTests('semi', semi => {
+        semi.addUiTest('keyboardEvent',  async run => {
+            run.appUi.content.listA.addDirect(
+                run.app.unboundG.createButton('activate test-app', () => {
+                }),
+                run.app.unboundG.createButton('switch off testMode', () => {
+                    run.app.testMode = false;
+                })
+            );
+            run.app.logG.toListOfStrings = true;
+            run.app.logG.toConsole = true;
+            run.app.entity.log('human-action: click \'activate test-app\'');
+            run.app.entity.log('human-test: when pressing keys, the according key events are logged.');
+            run.app.entity.log('human-action: click \'switch off testMode\'');
+            run.app.entity.log('human-test: now the keys are not logged.');
+        });
+        semi.addUiTest('contextIcon',  async run => {
+            let parent = await run.app.createText('parent');
+            parent.installListA();
+            let inContext = await run.app.createText('inContext');
+            let withoutContext = await run.app.createText('withoutContext');
+            let outOfContext = await run.app.createText('outOfContext');
+            let longText = await run.app.createText('longText-'.repeat(25));
+            inContext.context = inContext.getPath(parent);
+            longText.context = longText.getPath(parent);
+            outOfContext.context = outOfContext.getPath(await run.app.createText('aDummyContext'));
+            await parent.listA.add(inContext);
+            await parent.listA.add(outOfContext);
+            await parent.listA.add(withoutContext);
+            await parent.listA.add(longText);
+            await run.appUi.content.listA.add(parent);
+            await run.appUi.update();
+            let parentUi = run.appUi.content.uiA.listG.uisOfListItems[0];
+            assert_sameAs(parentUi.uiA.listG.uisOfListItems[0].uiA.headerG.contextIcon.innerText, '-');
+            assert_sameAs(parentUi.uiA.listG.uisOfListItems[1].uiA.headerG.contextIcon.innerText, '/');
+            assert_sameAs(parentUi.uiA.listG.uisOfListItems[2].uiA.headerG.contextIcon.innerText, '');
+            run.app.entity.log('human-test: The item withContext should show the appropriate contextIcon -');
+            run.app.entity.log('human-test: The contextIcon always appears left of the content - also for the long text.');
+        });
+        semi.addUiTest('contextAsSubitem',  async run => {
+            run.app.entity.uiA.editable = true;
+            let parent = await run.app.createText('parent');
+            parent.installListA();
+            let inContext = await run.app.createText('inContext');
+            let withoutContext = await run.app.createText('withoutContext');
+            let outOfContext = await run.app.createText('outOfContext');
+            inContext.context = inContext.getPath(parent);
+            outOfContext.context = outOfContext.getPath(await run.app.createText('aDummyContext'));
+            outOfContext.installListA();
+            await outOfContext.listA.add(await run.app.createText('aDummySubitem'));
+            await parent.listA.add(inContext);
+            await parent.listA.add(outOfContext);
+            await parent.listA.add(withoutContext);
+            await run.appUi.content.listA.add(parent);
+            await run.appUi.update();
+            let parentUi = run.appUi.content.uiA.listG.uisOfListItems[0];
+            assertFalse(await parentUi.uiA.listG.uisOfListItems[0].uiA.hasContextAsSubitem());
+            assert(await parentUi.uiA.listG.uisOfListItems[1].uiA.hasContextAsSubitem());
+            assertFalse(await parentUi.uiA.listG.uisOfListItems[2].uiA.hasContextAsSubitem());
+            //
+            assert(await parentUi.uiA.listG.uisOfListItems[1].uiA.headerBodyG.hasBodyContent());
+            run.app.entity.log('human-test: outOfContext shows "contextAsSubitem"');
+        });
+        semi.addUiTest('upload',  async run => {
+            let html = run.app.createEntityWithApp();
+            run.appUi.content.listA.addDirect(html);
+            html.codeG_html = textFileInput((contents : any) => {
+                run.appUi.content.listA.addDirect(run.app.unboundG.createText(contents));
+                run.appUi.update();
+            });
+            run.app.entity.log('human-action: Click on upload -> choose a text file');
+            run.app.entity.log('human-test: The text of the file appears.');
+        });
+        semi.addUiTest('download',  async run => {
+            let html = run.app.createEntityWithApp();
+            run.appUi.content.listA.addDirect(html);
+            const fileContent = 'foo123';
+            const fileName = 'testfile.txt';
+            html.codeG_html = downloadText(fileContent, fileName, 'download');
+            run.app.entity.log('human-action: Click on download');
+            run.app.entity.log('human-test: A text file is downloaded.');
+            run.app.entity.log('human-test: The content of the downloaded file is ' + fileContent);
+        });
+        semi.addUiTest('setWidth',  async run => {
+            let appA = run.appUi.getApp();
+            let html = appA.createEntityWithApp();
+            appA.uiA.content.listA.addDirect(html);
+            html.codeG_html = document.createElement('div');
+            let left = document.createElement('div');
+            html.codeG_html.appendChild(left);
+            let right = document.createElement('div');
+            html.codeG_html.appendChild(right);
+            html.codeG_html.style.display = 'flex';
+            html.codeG_html.style.width = '30rem';
+            html.codeG_html.style.height = '10rem';
+            html.codeG_html.style.border = 'solid';
+            left.style.width = '5rem'; // this is not 'strong' enough. You will need the setWidth function
+            right.contentEditable = 'true';
+            right.style.minWidth = '5rem';
+            right.style.border = 'solid';
+            right.innerText = 'Write some text here (multiple lines)!';
+
+            setWidth(left, '5rem');
+
+            appA.entity.log('human-action: write multiple lines in the text field');
+            appA.entity.log('human-test: The text does not move.');
+        });
+    });
+}
---
+++ client/src/test/tester_tester.ts
@@ -0,0 +1,74 @@
+import type {TestG_NestedTestsA} from "@/test/TestG_NestedTestsA";
+import {Entity} from "@/Entity";
+import {assert, assert_notSameAs, assert_sameAs, assertFalse} from "@/utils";
+
+export function tester_tester_add(tests : TestG_NestedTestsA) {
+    tests.addNestedTests('tester', testerTests => {
+        testerTests.addTestWithNestedTests('run', async run => {
+            let name = 'testName';
+            let test: Entity = run.app.entity.createCode(name, () => {
+                // test
+            });
+
+            let testRun: Entity = await test.testG_run();
+
+            assert(testRun.testRunA.resultG_success);
+            assert_sameAs(testRun.testRunA.test, test);
+        }, runTest => {
+            runTest.addTestWithNestedTests('withNestedTest', async run => {
+                let name = 'testName';
+                let test: Entity = run.app.entity.createCode(name, () => {
+                });
+                test.testG_installNestedTestsA();
+                let nestedTest = test.testG_nestedTestsA.add_withoutApp('nestedTest', async () => {
+                });
+
+                let testRun: Entity = await test.testG_run();
+
+                assert(testRun.testRunA.resultG_success, 'testRun->success');
+                assert_sameAs(testRun.testRunA.test, test);
+                let nestedTestRun = await testRun.testRunA.nestedRuns.listA.getResolved(0);
+                assert(nestedTestRun.testRunA.resultG_success, 'nestedTestRun->success');
+                assert_sameAs(nestedTestRun.testRunA.test, nestedTest);
+            }, withNestedTest => {
+                withNestedTest.add('failingNestedTest', async run => {
+                    let test: Entity = run.app.entity.createCode('foo', () => {
+                    });
+                    test.testG_installNestedTestsA();
+                    test.testG_nestedTestsA.add_withoutApp('nestedTest', async () => {
+                        assert(false);
+                    });
+
+                    let testRun: Entity = await test.testG_run();
+
+                    assertFalse(testRun.testRunA.resultG_success);
+                    assertFalse((await testRun.testRunA.nestedRuns.listA.getResolved(0)).testRunA.resultG_success);
+                });
+                withNestedTest.add('runWithoutNestedTests', async run => {
+                    let test: Entity = run.app.entity.createCode('foo', () => {
+                    });
+                    test.testG_installNestedTestsA();
+                    test.testG_nestedTestsA.add_withoutApp('nestedTest', async () => {
+                        assert(false);
+                    });
+
+                    let testRun: Entity = await test.testG_run(true);
+
+                    assert(testRun.testRunA.resultG_success);
+                    assert_sameAs(testRun.testRunA.nestedRuns, undefined);
+                });
+            });
+            runTest.add('failing', async run => {
+                let name = 'testName';
+                let test: Entity = run.app.entity.createCode(name, (testRun: Entity) => {
+                    assert(false);
+                });
+
+                let testRun: Entity = await test.testG_run();
+
+                assertFalse(testRun.testRunA.resultG_success);
+                assert_notSameAs(testRun.testRunA.resultG_error, undefined);
+            });
+        });
+    });
+}
