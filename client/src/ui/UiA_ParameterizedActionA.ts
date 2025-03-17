063283001348552d76f2eaed3bbe3f494e2c50b6
Felix <f.benz-baldas@hotmail.de>
2025-03-17 12:02:48 +0100

    parameterizedAction (intermediate step)

--- client/src/ui/UiA_ParameterizedActionA.ts
+++ client/src/ui/UiA_ParameterizedActionA.ts
@@ -9,6 +9,10 @@ export class UiA_ParameterizedActionA {
     async bodyContentG_update() {
         let bodyContent = this.entity.getApp().unboundG.createList();
         bodyContent.listA.addDirect(this.entity.getApp().unboundG.createText('bodyContentUi'));
+        let parameters = await this.entity.getApp().createList();
+        await parameters.set('name', await this.entity.getApp().createText(''));
+        bodyContent.listA.addDirect(parameters);
+        // TODO
         this.bodyContentUi = await this.entity.uiA.createSubUiFor(bodyContent);
     }
 }
